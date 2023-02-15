59cc279d41e5214e13c3c66b9588160c0c34f926
Alejandra Betancourt <feellingokay@gmail.com>
2023-02-15 18:55:07 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import CustomForm from './components/CustomForm';
+import EditForm from './components/EditForm';
+import  TaskList from './components/TaskList';
+import useLocalStorage from "./hooks/useLocalStorage"
+import ThemeSwitcher from "./components/ThemeSwitcher"
+
+function App() {
+  const [tasks, setTasks] = useLocalStorage("react-todo.tasks",[ ])
+  const [editedTask, setEditedTask] = useState(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const [previousFocusEl, setPreviousFocusEl]=useState(null)
+
+  const addTask=(task)=> {
+    setTasks(prevState=>[...prevState,task])
+  }
+     
+
+
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t=> t.id !== id))
+  }
+ 
+  const toggleTask = (id) => {
+    setTasks(prevState => prevState.map (t=>(t.id=== id ? {...t, checked: !t.checked}:t)))
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map (t=>(t.id=== task.id ? {...t, name: task.name}:t)))
+    closeEditMode()
+  }
+
+  const closeEditMode = () =>{
+    setIsEditing(false)
+    previousFocusEl.focus()
+  }
+
+
+  const enterEditedMode=(task)=>{
+    setEditedTask(task)
+    setIsEditing(true)
+    setPreviousFocusEl(document.activeElement)
+  }
+   
+
+
+  return (
+    <div className="container">
+      <header>
+        <h1> 👩‍💻 My Task List 💼 </h1>
+      </header>
+      {
+      isEditing &&( 
+        <EditForm
+        editedTask={editedTask}
+        updateTask={updateTask} />)
+      }
+      <CustomForm addTask={addTask}  />
+      {tasks && (<TaskList 
+      tasks={tasks}
+      deleteTask={deleteTask}
+      toggleTask={toggleTask} 
+      enterEditedMode={enterEditedMode} 
+      closeEditMode={closeEditMode} />)}
+<ThemeSwitcher/>
+      </div>
+  )
+}
+
+
+export default App
---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react"
+import styles from "./ThemeSwitcher.module.css"
+import useLocalStorage from "../hooks/useLocalStorage"
+import { XMarkIcon, SunIcon, MoonIcon, SwatchIcon} from "@heroicons/react/24/solid"
+
+const ThemeSwitcher = () => {
+  const [hue, setHue] = useLocalStorage("react-todo.color","240")
+
+  const defaultDark= window.matchMedia("(prefers-color-scheme:dark)").matches
+  const [theme, setTheme] = useLocalStorage("react-todo.theme", defaultDark ? "dark" : "light")
+  const [isColorPicking, setIsColorPicking] = useState(false)
+
+  
+  useEffect(()=>{
+    document.documentElement.setAttribute("color-scheme", theme)
+  }, [theme])
+
+  useEffect(()=>{
+    document.documentElement.style.setProperty("--_hue", hue)
+  }, [hue])
+
+
+
+  return (
+    <aside 
+    className={styles.wrapper} 
+    style= {{
+      backgroundColor: isColorPicking
+      ? "hsl(var(--muted)/.6)"
+      : "transparent"
+    }}>
+ { isColorPicking 
+    ?(
+      <>
+      <button
+      className={`btn ${styles.close}`} 
+      aria-label= "Close color picking mode"
+      onClick={()=>setIsColorPicking(false)}
+      >
+        <XMarkIcon/>
+      </button>
+      <input 
+      className={styles.picker}
+      type= "range"
+      min="0"
+      max="360"
+      aria-label="Change color theme slider"
+      value={hue}
+      onInput={(e)=> setHue(e.target.value)} />
+
+      
+      </>
+    )
+    :(
+      <div className={styles.btns} >
+        <button 
+        className="btn"
+        aria-label={`Change theme to ${theme === "light" ? "dark" : "light"} mode`} 
+        role="switch"
+        onClick={()=>setTheme(theme === "light" ? "dark" : "light")}>
+          {theme === "dark" ?<SunIcon/> : <MoonIcon/>}
+          
+        </button>
+        <button className="btn"
+        aria-label="Enable picking mode"
+        onClick={()=>setIsColorPicking(true)}>
+          <SwatchIcon/> 
+        </button>
+
+      </div>
+    )
+ }
+    </aside>
+  )
+}
+export default ThemeSwitcher
+ 
